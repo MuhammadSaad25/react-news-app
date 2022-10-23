7745dfc052df7aae6367f9e23d76c88fc9f5a1ca
Muhammad Saad <muhammadsaad2522@gmail.com>
2022-10-23 05:24:53 -0700

    funcitions complete

--- src/components/news/index.jsx
+++ src/components/news/index.jsx
@@ -7,6 +7,36 @@ function News() {
 
   const [data, setData] = useState([]);
   const [query, setQuery] = useState("");
+  const [isloading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    function getTrendingNews() {
+      const options = {
+        method: 'GET',
+        url: 'https://bing-news-search1.p.rapidapi.com/news',
+        params: { safeSearch: 'Off', textFormat: 'Raw' },
+        headers: {
+          'X-BingApis-SDK': 'true',
+          'X-RapidAPI-Key': '7d22d9a396msh925a60d8fa0cfbfp1e1d6ajsn178f1b726d4d',
+          'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com'
+        }
+      };
+      axios.request(options)
+        .then(function (response) {
+          setData(response.data.value)
+          console.log(response.data);
+        }).catch(function (error) {
+          console.error(error);
+        });
+    }
+
+    getTrendingNews();
+
+
+  }, [])
+
+
+
 
   const getNews = (e) => {
     e?.preventDefault();
@@ -22,10 +52,12 @@ function News() {
         'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com'
       }
     };
-
+    setIsLoading(true)
     axios
       .request(options)
       .then(function (response) {
+        setIsLoading(false)
+
         console?.log(response?.data?.value);
 
         setData(response?.data?.value);
@@ -33,6 +65,7 @@ function News() {
 
       })
       .catch(function (error) {
+        setIsLoading(false)
         console.error(error);
       });
   }
@@ -51,13 +84,18 @@ function News() {
 
         <button type="submit">Get News</button>
 
+        <div>
+          {(isloading) ? "loading ..." : ""}
+        </div>
+
         <div>{data.map(eachPost => (
           <div key={eachPost?.name}>
             <div>{moment(eachPost?.datePublished)?.format('DD MMMM  h:mm a')}</div>
             <h1>{eachPost?.name}</h1>
             <p>{eachPost?.description}</p>
-            <img src={eachPost?.image?.thumbnail?.contentUrl?.replace("&pid=News","")?.replace("pid=News&","")?.replace("pid=News","")} alt="" />
-            <a href={eachPost?.url}>Read More</a>
+            <img src={eachPost?.image?.thumbnail?.contentUrl?.replace("&pid=News", "")?.replace("pid=News&", "")?.replace("pid=News", "")} alt="" />
+            <br />
+            <a href={eachPost?.url} target="_blank" rel="noreferrer">Read More</a>
 
           </div>
         ))}
